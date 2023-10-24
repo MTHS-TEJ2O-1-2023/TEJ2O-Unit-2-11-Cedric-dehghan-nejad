27f64f7686f24b9447943a535afe0356b6ad652c
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-10-24 13:13:16 -0400

    typescrpt change 3

--- main.ts
+++ main.ts
@@ -10,31 +10,35 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 //variable 
-let number1: number = randint(0, 99)
-let number2: number = randint(0, 99)
+let number1: number 
+let number2: number 
 
-input.onButtonPressed(Button.A, function() {
-    basic.showString("#")
-    basic.showNumber(number1)
+//randint 
+number1 = randint (0, 99)
+number2 = randint (0, 99)
+
+input.onButtonPressed(Button.A, function () {
+  basic.showString('#')
+  basic.showNumber(number1)
 })
 
 input.onButtonPressed(Button.B, function () {
-    basic.showString("#")
-    basic.showNumber(number2)
+  basic.showString('#')
+  basic.showNumber(number2)
 })
 
-input.onGesture(Gesture.Shake, function() {
-    if (number1 < number2) {
+input.onGesture(Gesture.Shake, function () {
+  if (number1 < number2) {
     basic.showNumber(number1)
-    basic.showString("<")
+    basic.showString('<')
     basic.showNumber(number2)
     basic.showIcon(IconNames.Happy)
   }
   else {
     (number1 > number2)
     basic.showNumber(number1)
-    basic.showString("<")
+    basic.showString('<')
     basic.showNumber(number2)
     basic.showIcon(IconNames.Sad)
-    }
+  }
 })
