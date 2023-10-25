3864da628733f971f1df5775ed195f89a62529c4
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-10-24 22:04:11 -0400

    123456789

--- main.ts
+++ main.ts
@@ -5,18 +5,18 @@
  * This program ...
 */
 
-//setup
+// setup
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
- //variable 
-let number1: number 
-let number2: number 
+// variable
+let number1: number
+let number2: number
 
- //randint 
-number1 = randint (0,99)
-number2 = randint (0,99)
- // show number1
+// randint
+number1 = randint(0, 99)
+number2 = randint(0, 99)
+// show number1
 input.onButtonPressed(Button.A, function () {
   basic.showString('#')
   basic.showNumber(number1)
@@ -26,15 +26,16 @@ input.onButtonPressed(Button.B, function () {
   basic.showString('#')
   basic.showNumber(number2)
 })
- // on Gesture show number1 < number 2
+// on Gesture show number1 < number 2
 input.onGesture(Gesture.Shake, function () {
   if (number1 < number2) {
     basic.showNumber(number1)
     basic.showString('<')
     basic.showNumber(number2)
-    basic.showIcon(IconNames.Happy) }
- // on Gesture show number1 > number 2
-  else {
+    basic.showIcon(IconNames.Happy) 
+
+  // on Gesture show number1 > number 2
+  } else {
     basic.showNumber(number1)
     basic.showString('>')
     basic.showNumber(number2)
